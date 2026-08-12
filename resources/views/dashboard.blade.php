4afa7fa524d8607a09c094fe5f370ad3aa838727
Ghrysh <gispi.desu@gmail.com>
2026-08-12 13:54:56 +0700

    fix: CORS issue for flatpickr css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Flatpickr CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/airbnb.css" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/airbnb.css" crossorigin="anonymous">
     <!-- Custom CSS for exact styling -->
     <style>
         .grid-bg {
